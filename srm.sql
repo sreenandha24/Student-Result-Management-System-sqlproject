d7772d5eb41e77a29b95f8e87c8ecf89f71ffa3a
sreenandha24 <83593858+sreenandha24@users.noreply.github.com>
2021-05-03 19:49:37 +0530

    Add files via upload

---
+++ srm.sql
@@ -0,0 +1,295 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 24, 2018 at 07:27 AM
+-- Server version: 10.1.36-MariaDB
+-- PHP Version: 7.2.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `srm`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `id` int(11) NOT NULL,
+  `UserName` varchar(100) NOT NULL,
+  `Password` varchar(100) NOT NULL,
+  `updationDate` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`id`, `UserName`, `Password`, `updationDate`) VALUES
+(1, 'admin', '21232f297a57a5a743894a0e4a801fc3', '2018-10-23 18:10:50'),
+(2, 'saxenapriyansh', '5e5693106f98422d9e1fa019202c1890', '2018-10-23 18:07:00');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblclasses`
+--
+
+CREATE TABLE `tblclasses` (
+  `id` int(11) NOT NULL,
+  `ClassName` varchar(80) DEFAULT NULL,
+  `ClassNameNumeric` int(4) NOT NULL,
+  `Section` varchar(5) NOT NULL,
+  `CreationDate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `UpdationDate` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tblclasses`
+--
+
+INSERT INTO `tblclasses` (`id`, `ClassName`, `ClassNameNumeric`, `Section`, `CreationDate`, `UpdationDate`) VALUES
+(1, 'First', 1, 'C', '2018-10-23 17:21:20', '2018-10-23 18:51:56'),
+(2, 'Second', 2, 'A', '2018-10-23 17:21:20', '2018-10-23 17:21:20'),
+(4, 'Fourth', 4, 'C', '2018-10-23 17:21:20', '2018-10-23 17:21:20'),
+(5, 'Sixth', 6, 'A', '2018-10-23 17:21:20', '2018-10-23 18:52:27'),
+(6, 'Sixth', 6, 'B', '2018-10-23 17:21:20', '2018-10-23 17:21:20'),
+(7, 'Seventh', 7, 'B', '2018-10-23 17:21:20', '2018-10-23 17:21:20'),
+(8, 'Eight', 8, 'A', '2018-10-23 17:21:20', '2018-10-23 17:21:20');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblresult`
+--
+
+CREATE TABLE `tblresult` (
+  `id` int(11) NOT NULL,
+  `StudentId` int(11) DEFAULT NULL,
+  `ClassId` int(11) DEFAULT NULL,
+  `SubjectId` int(11) DEFAULT NULL,
+  `marks` int(11) DEFAULT NULL,
+  `PostingDate` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `UpdationDate` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tblresult`
+--
+
+INSERT INTO `tblresult` (`id`, `StudentId`, `ClassId`, `SubjectId`, `marks`, `PostingDate`, `UpdationDate`) VALUES
+(2, 1, 1, 2, 100, '2017-08-24 17:54:09', '2017-08-28 18:34:32'),
+(3, 1, 1, 1, 80, '2017-08-24 17:54:09', '2017-08-28 18:34:25'),
+(4, 1, 1, 5, 78, '2017-08-24 17:54:09', '2017-08-28 18:34:26'),
+(5, 1, 1, 4, 60, '2017-08-24 17:54:09', '2017-08-28 18:34:26'),
+(6, 2, 4, 2, 90, '2017-08-28 18:38:18', NULL),
+(7, 2, 4, 1, 75, '2017-08-28 18:38:18', NULL),
+(8, 2, 4, 5, 56, '2017-08-28 18:38:18', '2017-08-28 19:26:42'),
+(9, 2, 4, 4, 80, '2017-08-28 18:38:18', '2017-08-28 19:26:42'),
+(10, 4, 7, 2, 54, '2017-08-28 18:56:21', '2017-08-28 19:03:10'),
+(11, 4, 7, 1, 85, '2017-08-28 18:56:21', NULL),
+(12, 4, 7, 5, 55, '2017-08-28 18:56:21', '2017-08-28 19:03:10'),
+(13, 4, 7, 7, 65, '2017-08-28 18:56:21', '2017-08-28 19:03:10'),
+(14, 5, 8, 2, 75, '2017-08-28 19:25:07', NULL),
+(15, 5, 8, 1, 56, '2017-08-28 19:25:07', NULL),
+(16, 5, 8, 5, 52, '2017-08-28 19:25:07', NULL),
+(17, 5, 8, 4, 80, '2017-08-28 19:25:07', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblstudents`
+--
+
+CREATE TABLE `tblstudents` (
+  `StudentId` int(11) NOT NULL,
+  `StudentName` varchar(100) NOT NULL,
+  `RollId` varchar(100) NOT NULL,
+  `StudentEmail` varchar(100) NOT NULL,
+  `Gender` varchar(10) NOT NULL,
+  `DOB` varchar(100) NOT NULL,
+  `ClassId` int(11) NOT NULL,
+  `RegDate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `UpdationDate` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  `Status` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tblstudents`
+--
+
+INSERT INTO `tblstudents` (`StudentId`, `StudentName`, `RollId`, `StudentEmail`, `Gender`, `DOB`, `ClassId`, `RegDate`, `UpdationDate`, `Status`) VALUES
+(1, 'Abhishek Choudhary', '46456', 'abhishek.cho472@gmail.com', 'Male', '1997-07-09', 1, '2018-10-12 10:30:57', '2018-10-23 18:44:37', 1),
+(2, 'Akshat Maheshwari', '10861', 'aks3d76@gmail.com', 'Male', '1997-06-11', 4, '2018-10-19 18:42:28', '2018-10-23 18:46:16', 0),
+(3, 'Mohit Kumar', '2626', 'humar.mohit570@gmail.com', 'Male', '2014-08-06', 6, '2018-10-19 18:42:28', '2018-10-23 18:48:34', 1),
+(4, 'Divyanshu Tripathi', '990', 'hidivyanshu@gmail.com', 'Male', '1997-02-03', 7, '2018-10-19 18:42:28', '2018-10-23 18:47:54', 1),
+(5, 'Deepankar Singh', '122', 'singhdeepankar111@gmail.com', 'Male', '1997-02-03', 8, '2018-10-19 18:42:28', '2018-10-23 18:47:21', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblsubjectcombination`
+--
+
+CREATE TABLE `tblsubjectcombination` (
+  `id` int(11) NOT NULL,
+  `ClassId` int(11) NOT NULL,
+  `SubjectId` int(11) NOT NULL,
+  `status` int(1) DEFAULT NULL,
+  `CreationDate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `Updationdate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tblsubjectcombination`
+--
+
+INSERT INTO `tblsubjectcombination` (`id`, `ClassId`, `SubjectId`, `status`, `CreationDate`, `Updationdate`) VALUES
+(3, 2, 5, 1, '2017-06-07 11:16:56', '2017-06-07 11:16:56'),
+(4, 1, 2, 1, '2017-06-12 06:46:32', '2017-06-12 06:46:32'),
+(5, 1, 4, 1, '2017-06-12 06:46:35', '2017-06-12 06:46:35'),
+(6, 1, 5, 1, '2017-06-12 06:46:40', '2017-06-12 06:46:40'),
+(8, 4, 4, 1, '2017-08-26 03:21:27', '2017-08-26 03:21:27'),
+(10, 4, 1, 1, '2017-08-26 03:22:05', '2017-08-26 03:22:05'),
+(12, 4, 2, 1, '2017-08-26 03:22:15', '2017-08-26 03:22:15'),
+(13, 4, 5, 1, '2017-08-26 03:22:20', '2017-08-26 03:22:20'),
+(14, 6, 1, 1, '2017-08-28 18:44:06', '2017-08-28 18:44:06'),
+(15, 6, 2, 1, '2017-08-28 18:44:12', '2017-08-28 18:44:12'),
+(16, 6, 4, 1, '2017-08-28 18:44:18', '2017-08-28 18:44:18'),
+(17, 6, 6, 1, '2017-08-28 18:44:23', '2017-08-28 18:44:23'),
+(18, 7, 1, 1, '2017-08-28 18:53:12', '2017-08-28 18:53:12'),
+(19, 7, 7, 1, '2017-08-28 18:53:19', '2017-08-28 18:53:19'),
+(20, 7, 2, 1, '2017-08-28 18:53:38', '2017-08-28 18:53:38'),
+(21, 7, 6, 1, '2017-08-28 18:53:44', '2017-08-28 18:53:44'),
+(22, 7, 5, 0, '2017-08-28 18:53:50', '2017-08-28 18:53:50'),
+(23, 8, 1, 1, '2017-08-28 19:22:25', '2017-08-28 19:22:25'),
+(24, 8, 2, 1, '2017-08-28 19:22:31', '2017-08-28 19:22:31'),
+(25, 8, 4, 1, '2017-08-28 19:22:36', '2017-08-28 19:22:36'),
+(26, 8, 6, 1, '2017-08-28 19:22:42', '2017-08-28 19:22:42'),
+(27, 8, 5, 0, '2017-08-28 19:22:47', '2017-08-28 19:22:47');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblsubjects`
+--
+
+CREATE TABLE `tblsubjects` (
+  `id` int(11) NOT NULL,
+  `SubjectName` varchar(100) NOT NULL,
+  `SubjectCode` varchar(100) NOT NULL,
+  `Creationdate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `UpdationDate` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tblsubjects`
+--
+
+INSERT INTO `tblsubjects` (`id`, `SubjectName`, `SubjectCode`, `Creationdate`, `UpdationDate`) VALUES
+(1, 'Maths', 'MTH01', '2018-10-23 17:21:20', '2018-10-23 17:21:20'),
+(2, 'English', 'ENG11', '2018-10-23 17:21:20', '2018-10-23 17:21:20'),
+(4, 'Science', 'SC1', '2018-10-23 17:21:20', '2018-10-23 17:21:20'),
+(5, 'Music', 'MS', '2018-10-23 17:21:20', '2018-10-23 17:21:20'),
+(6, 'Social Studies', 'SS08', '2018-10-23 17:21:20', '2018-10-23 17:21:20'),
+(7, 'Physics', 'PH03', '2018-10-23 17:21:20', '2018-10-23 17:21:20'),
+(8, 'Chemistry', 'CH65', '2018-10-23 17:21:20', '2018-10-23 17:21:20');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tblclasses`
+--
+ALTER TABLE `tblclasses`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tblresult`
+--
+ALTER TABLE `tblresult`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tblstudents`
+--
+ALTER TABLE `tblstudents`
+  ADD PRIMARY KEY (`StudentId`);
+
+--
+-- Indexes for table `tblsubjectcombination`
+--
+ALTER TABLE `tblsubjectcombination`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tblsubjects`
+--
+ALTER TABLE `tblsubjects`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `tblclasses`
+--
+ALTER TABLE `tblclasses`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `tblresult`
+--
+ALTER TABLE `tblresult`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+
+--
+-- AUTO_INCREMENT for table `tblstudents`
+--
+ALTER TABLE `tblstudents`
+  MODIFY `StudentId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `tblsubjectcombination`
+--
+ALTER TABLE `tblsubjectcombination`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+
+--
+-- AUTO_INCREMENT for table `tblsubjects`
+--
+ALTER TABLE `tblsubjects`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
